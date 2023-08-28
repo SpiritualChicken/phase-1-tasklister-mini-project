d8220da8918650cdbbf27c8829420d1c44d5d00c
Charlie <cnewlinds@gmail.com>
2023-08-28 11:47:09 +1000

    working todo form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  createToDo(e.target.querySelector("#new-task-description").value);
+  form.reset();
+  })
 });
+
+
+function createToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x';
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  document.querySelector('#tasks').append(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
